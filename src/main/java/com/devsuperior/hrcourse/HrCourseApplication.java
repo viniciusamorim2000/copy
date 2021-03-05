93670961e9c27f42ad1508ce77b249550a5dc205
Vinícius Amorim da Silva <vinicius.amorim@basis.com.br>
2021-03-05 10:28:57 -0300

    Criando projeto

---
+++ src/main/java/com/devsuperior/hrcourse/HrCourseApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.hrcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrCourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/hrcourse/HrCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.hrcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
